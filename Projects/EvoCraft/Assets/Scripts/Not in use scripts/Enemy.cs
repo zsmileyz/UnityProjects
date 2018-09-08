901d9626c9335eb0d767c4e1948dd742a853885b
zsmileyz <43080429+zsmileyz@users.noreply.github.com>
2018-09-08 18:19:29 -0400

    Player.Enemy.Colliders.Scripts
    I added those objects plus the Scripts. Working on the spawners now

---
+++ Projects/EvoCraft/Assets/Scripts/Not in use scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+        transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+	}
+
+
+}
---
+++ Projects/EvoCraft/Assets/Scripts/Not in use scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+        Controller();      
+    }
+
+        void Controller()
+        {
+         if (Input.GetKey("d"))
+         {
+            transform.Translate(speed * Time.deltaTime, 0f, 0f);
+         }
+
+         if (Input.GetKey("a"))
+         {
+            transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+         }
+
+         if (Input.GetKey("w"))
+         {
+            transform.Translate(0f,speed * Time.deltaTime, 0f);
+         }
+
+         if (Input.GetKey("s"))
+         {
+            transform.Translate(0f, -speed * Time.deltaTime, 0f);
+         }
+        }
+    
+
+    
+
+
+
+
+
+
+
+}
