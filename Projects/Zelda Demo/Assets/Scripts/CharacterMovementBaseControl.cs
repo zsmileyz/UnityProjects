24fa1334ecdfbdfbb8659b747de96d5f07f31e3a
zsmileyz <43080429+zsmileyz@users.noreply.github.com>
2018-09-13 17:33:07 -0400

    Zelda Stuff

---
+++ Projects/Zelda Demo/Assets/Scripts/CharacterMovementBaseControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementBaseControl : MonoBehaviour 
+{
+    protected CharacterMovementModel m_MovementModel;
+
+    private void Awake()
+    {
+        m_MovementModel = GetComponent<CharacterMovementModel>();
+    }
+
+    protected void SetDirection(Vector2 direction)
+    {
+        m_MovementModel.SetDirection(direction);
+    }
+
+
+
+	
+	
+}
---
+++ Projects/Zelda Demo/Assets/Scripts/CharacterMovementKeyboardControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementKeyboardControl : CharacterMovementBaseControl
+{
+
+
+
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+        UpdateDirection();
+	}
+	
+    void UpdateDirection()
+    {
+        Vector2 newDirection = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            newDirection.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            newDirection.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            newDirection.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            newDirection.x = 1;
+        }
+
+        SetDirection(newDirection);
+    }
+	
+}
