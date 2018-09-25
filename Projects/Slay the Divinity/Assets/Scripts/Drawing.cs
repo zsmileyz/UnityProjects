c6a538da762b75c94ba4f0b5e17b11b07111c418
zsmileyz <43080429+zsmileyz@users.noreply.github.com>
2018-09-25 19:51:19 -0400

    Added podo of Drawing

---
+++ Projects/Slay the Divinity/Assets/Scripts/Drawing.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawing : MonoBehaviour 
+{
+    public GameObject card;
+    public GameObject card1;
+
+
+	void Start () 
+	{
+        StartCoroutine("StartingDraw");
+    }
+	
+	void Update () 
+	{
+
+        
+    }
+
+
+
+    IEnumerator StartingDraw()
+    {
+        Instantiate(card,transform,transform.parent);
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(card1, transform, transform.parent);
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(card, transform, transform.parent);
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(card1, transform, transform.parent);
+        yield return new WaitForSeconds(0.5f);
+        Instantiate(card, transform, transform.parent);
+        Debug.Log("Drawing Complete!!");
+    }    
+	
+}
