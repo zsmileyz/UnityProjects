e4f7f562ec5cda60a24756f1c20a9246a7b9e8c2
zsmileyz <43080429+zsmileyz@users.noreply.github.com>
2018-12-13 10:32:10 -0500

    Added movement

---
+++ Projects/Fall Damage/Assets/Scripts/Player_1_Controller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_1_Controller : MonoBehaviour 
+{
+
+    public float speed = 5f;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+        Controller();
+	}
+
+    void Controller()
+    {
+        if (Input.GetKey("l"))
+        {
+            transform.Translate(speed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey("j"))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0f, 0f);
+        }
+
+        if (Input.GetKey("i"))
+        {
+            transform.Translate(0f, speed * Time.deltaTime, 0f);
+        }
+
+        if (Input.GetKey("k"))
+        {
+            transform.Translate(0f, -speed * Time.deltaTime, 0f);
+        }
+    }
+
+}
---
+++ Projects/Fall Damage/Assets/Scripts/Player_1_Stats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_1_Stats : MonoBehaviour 
+{
+
+    public float hitForce = 0.5f;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+	
+	
+	
+}
