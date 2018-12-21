4f4af440f74bc23b7c893ae15878669c404ad815
zsmileyz <43080429+zsmileyz@users.noreply.github.com>
2018-12-21 10:30:06 -0500

    Added timer for cubes/fixed cubes.Added human models.

---
+++ Projects/Fall Damage/Assets/Scripts/First_4_Drop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First_4_Drop : MonoBehaviour
+{
+
+    float speed = 2;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        StartCoroutine(Droptime());
+    }
+
+    IEnumerator Droptime()
+    {
+        yield return new WaitForSeconds(2);
+        Drop();
+        Debug.Log("test");
+        yield return new WaitForSeconds(5);
+        Destroy(gameObject);
+        Debug.Log("death cube");
+    }
+
+    void Drop()
+    {
+        transform.Translate(0f, -speed * Time.deltaTime, 0f);
+    }
+
+}
