36a1db63cb58de07f84de4c932965863fff0da03
ZSmileyz <zsmileyz.devteam@gmail.com>
2019-01-03 11:43:34 -0500

    Added visual effect for Umbrella push and changed umbrella

---
+++ Projects/Rubble Royal/Assets/Scripts/Battle/Spawner3.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner3 : MonoBehaviour
+{
+    public GameObject fan;
+    public float time = 5f;
+
+    void Start()
+    {
+        StartCoroutine(Drop());
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Fan()
+    {
+        Instantiate(fan, transform.position, Quaternion.identity);
+    }
+
+    IEnumerator Drop()
+    {
+        yield return new WaitForSeconds(time);
+        Fan();
+        Debug.Log("drop!");
+    }
+}
