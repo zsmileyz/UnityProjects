4957296582d020de5484978631c03a8d3419d6ff
zsmileyz <43080429+zsmileyz@users.noreply.github.com>
2019-01-07 21:11:11 -0500

    Added respawner to player 2 and win game setup.

---
+++ Projects/Rubble Royal/Assets/Scripts/Battle/Player 2/Player_2_Controller.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_2_Controller : MonoBehaviour
+{
+    public float red = 0;
+    public float win = 5;
+
+    public Text Score;
+
+    public bool is_Dead = false;
+
+    [SerializeField] private Transform playerStartingPoint;
+    [SerializeField] private Transform respawnPoint;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+       
+    }
+    
+    IEnumerator respawner()
+    {
+        yield return new WaitForSeconds(2f);
+        red += 1;
+        Score.text = red.ToString();
+        is_Dead = true;
+        playerStartingPoint.transform.position = respawnPoint.transform.position;
+        Debug.Log(red);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Pit"))
+        {
+            StartCoroutine(respawner());
+           
+            
+            /*red += 1;
+            Score.text = red.ToString();
+            is_Dead = true;
+            playerStartingPoint.transform.position = respawnPoint.transform.position;
+            Debug.Log(red);*/
+        }
+        if (red >= win)
+        {
+            Score.text = "You Win!";
+            Destroy(gameObject);
+            Time.timeScale = 0;
+        }
+    }
+}
