180d83c7ef4fa5aa0ddf5dd816a90d5d56cd5bca
zsmileyz <43080429+zsmileyz@users.noreply.github.com>
2019-09-01 19:37:48 -0400

    Added bad movement

---
+++ Projects/CUBEKNIGHT/Assets/Scripts/Character_Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Movement : MonoBehaviour
+{
+    public float movementSpeed = 0.01f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Controller();
+    }
+
+    void Controller()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0,movementSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -movementSpeed, 0);
+        }
+
+
+        
+    }
+}
