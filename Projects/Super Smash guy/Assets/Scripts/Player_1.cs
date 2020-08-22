139c59d7a9597968ee568bf2371bdf1118bab1b9
zsmileyz <43080429+zsmileyz@users.noreply.github.com>
2020-08-22 16:23:51 -0400

    Added character/map and basic movement. (bug)

---
+++ Projects/Super Smash guy/Assets/Scripts/Player_1.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_1 : MonoBehaviour
+{
+
+    float Movementspeed = 10f;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Controller();
+    }
+
+    void Controller()
+    {
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Movementspeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(-Movementspeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(0, Movementspeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(0, -Movementspeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("space"))
+        {
+            transform.Translate(0, 0, Movementspeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Movementspeed = 20f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Movementspeed = 10f;
+        }
+    }
+
+
+
+
+
+
+
+}
